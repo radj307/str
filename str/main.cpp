47eddf0a3fd0386617be86a112e2745470f2580e
radj307 <radj307@gmail.com>
2022-02-02 17:01:12 -0500

    update build files

--- str/main.cpp
+++ str/main.cpp
@@ -1,4 +1,20 @@
 ﻿
+
+#include <hasPendingDataSTDIN.h>
+
+#include <iostream>
+#include <string>
+
+inline std::string get_stdin()
+{
+	if (hasPendingDataSTDIN()) {
+		std::string str;
+		for (std::string buf{}; std::cin >> buf; str += buf) {}
+		return str;
+	}
+	return{};
+}
+
 int main(const int argc, char** argv)
 {
 
